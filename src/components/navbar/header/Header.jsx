697d0177f706d21cb3e4f141bbe22fba7d790ca4
Kevin Li <kevjli411@gmail.com>
2022-09-09 20:40:00 -0400

    added to header

--- src/components/navbar/header/Header.jsx
+++ src/components/navbar/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCar, faHotel, faPlane } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDays, faCar, faHotel, faPeopleGroup, faPlane } from '@fortawesome/free-solid-svg-icons'
 import "./header.css"
 
 const Header = () => {
@@ -24,6 +24,20 @@ const Header = () => {
             <h1 className="headerTitle">Want free savings? It's literally free money.</h1>
             <p className="headerDesc">Find the cheapest hotel stays, car rentals, and airplane flight tickets with a free KevBooking account</p>
             <button className="headerButton">Sign In / Sign Up</button>
+            <div className="headerSearch">
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faHotel} className="headerIcon"/>
+                    <input type="text" placeholder='Where to?' className='headerSearchInput'/>
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
+                    <span className='headerSearchText'> date to date</span>
+                </div>
+                <div className="headerSearchItem">
+                    <FontAwesomeIcon icon={faPeopleGroup} className="headerIcon"/>
+                    <span className='headerSearchText'> 2 adults 2 children 1 room</span>
+                </div>
+            </div>
         </div>
     </div>
   )
